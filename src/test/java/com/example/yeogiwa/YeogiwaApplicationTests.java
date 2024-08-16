36f25bf9e89f0c5573e332dd3459c5528722651f
mathpaul3 <mathpaul3@gmail.com>
2024-08-16 19:09:39 +0900

    fix: remove test

--- src/test/java/com/example/yeogiwa/YeogiwaApplicationTests.java
+++ src/test/java/com/example/yeogiwa/YeogiwaApplicationTests.java
@@ -1,22 +1,6 @@
 package com.example.yeogiwa;
-
-import com.example.yeogiwa.controller.UserControllerTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.Test;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class YeogiwaApplicationTests {
-	@Autowired
-	private UserControllerTest userControllerTest;
-
-	@Test
-	void contextLoads() throws Exception{
-		assertThat(userControllerTest).isNotNull();
-	}
-
 }
