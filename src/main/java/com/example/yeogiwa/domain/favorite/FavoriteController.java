4f151ad8efb64bc434478850aec2b8d071c8779f
choiseohyeon <csh200310@naver.com>
2024-09-25 20:29:07 +0900

    [fix] content 값 추가

--- src/main/java/com/example/yeogiwa/domain/favorite/FavoriteController.java
+++ src/main/java/com/example/yeogiwa/domain/favorite/FavoriteController.java
@@ -26,9 +26,9 @@ public class FavoriteController {
 
     @Operation(summary = "즐겨찾기 추가", description = "사용자와 이벤트 정보를 받아 즐겨찾기 추가, 즐겨찾기 ID 반환")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "즐겨찾기 추가 성공"),
-            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
-            @ApiResponse(responseCode = "404", description = "사용자 또는 이벤트를 찾을 수 없음")
+            @ApiResponse(responseCode = "200", description = "즐겨찾기 추가 성공", content = @Content(schema = @Schema(implementation = Long.class))),
+            @ApiResponse(responseCode = "400", description = "잘못된 요청", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "404", description = "사용자 또는 이벤트를 찾을 수 없음", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @PostMapping()
     public Long addFavorite(Authentication authentication, @RequestParam String eventId) {
@@ -39,36 +39,36 @@ public class FavoriteController {
 
     @Operation(summary = "즐겨찾기 삭제", description = "즐겨찾기 ID를 받아 즐겨찾기 삭제")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "즐겨찾기 삭제 성공"),
-            @ApiResponse(responseCode = "404", description = "즐겨찾기를 찾을 수 없음")
+            @ApiResponse(responseCode = "200", description = "즐겨찾기 삭제 성공", content = @Content(schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "404", description = "즐겨찾기를 찾을 수 없음", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @DeleteMapping("/{favoriteId}")
     public void removeFavorite(@PathVariable Long favoriteId) {
         favoriteService.removeFavorite(favoriteId);
     }
 
-    @Operation(summary = "즐겨찾기 목록 조회", description = "현재 로그인한 사용자의 즐겨찾기 목록 조회")
+    @Operation(summary = "즐겨찾기 목록 조회", description = "사용자의 즐겨찾기 목록 조회")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "즐겨찾기 목록 조회 성공", content = @Content(array = @ArraySchema(schema = @Schema(implementation = FavoriteDto.class)))),
-            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음")
+            @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @GetMapping("/list")
     public ResponseEntity<List<FavoriteDto>> getFavoritesByUser(Authentication authentication) {
         PrincipalDetails user = (PrincipalDetails) authentication.getPrincipal();
         List<FavoriteDto> favoriteList = favoriteService.getFavoritesByUser(user.getUserId());
-        return ResponseEntity.ok(favoriteList);
+        return ResponseEntity.status(200).body(favoriteList);
     }
 
     @Operation(summary = "즐겨찾기 단일 조회", description = "이벤트 ID를 받아 해당 이벤트의 즐겨찾기 정보를 조회")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "즐겨찾기 단일 조회 성공"),
-            @ApiResponse(responseCode = "404", description = "즐겨찾기를 찾을 수 없음")
+            @ApiResponse(responseCode = "200", description = "즐겨찾기 단일 조회 성공", content = @Content(schema = @Schema(implementation = FavoriteDto.class))),
+            @ApiResponse(responseCode = "404", description = "즐겨찾기를 찾을 수 없음", content = @Content(schema = @Schema(implementation = String.class)))
     })
     @GetMapping("/event/{eventId}")
     public ResponseEntity<FavoriteDto> getFavorite(Authentication authentication, @PathVariable String eventId) {
         PrincipalDetails user = (PrincipalDetails) authentication.getPrincipal();
         FavoriteDto favorite = favoriteService.getFavorite(user.getUserId(), eventId);
-        return ResponseEntity.ok(favorite);
+        return ResponseEntity.status(200).body(favorite);
     }
 }
 
